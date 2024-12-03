f8242f2ab0dd8c14dbafbecc384e5ebf41b5c8e7
MarcosDevRamos <santosramosmarcosvinicius@gmail.com>
2024-12-03 11:50:38 -0400

    removendo o jquery e adicionando .catch

--- main.js
+++ main.js
@@ -1,25 +1,33 @@
-$(document).ready(function(){
+document.addEventListener('DOMContentLoaded', function() {
 
-    const nameElement = $('#name');
-    const usernameElement = $('#username');
-    const avatarElement = $('#avatar');
-    const reposElement = $('#repos');
-    const followersElement = $('#followers');
-    const followingElement = $('#following');
-    const linkElement = $('#link');
+    const nameElement = document.querySelector('#name');
+    const usernameElement = document.querySelector('#username');
+    const avatarElement = document.querySelector('#avatar');
+    const reposElement = document.querySelector('#repos');
+    const followersElement = document.querySelector('#followers');
+    const followingElement = document.querySelector('#following');
 
-    const endpoint = 'https://api.github.com/users/marcosVSRamos';
-        
-        $.ajax(endpoint).done(function(resposta) {
+    fetch('https://api.github.com/users/marcosVSRamos')
+        .then(function(res) {
 
-            nameElement.text(resposta.name);
-            usernameElement.text(resposta.login);
-            avatarElement.attr('src', resposta.avatar_url);
-            followersElement.text(resposta.followers);
-            followingElement.text(resposta.following);
-            reposElement.text(resposta.public_repos);
-            linkElement.attr('href', resposta.html_url);
-            
+            if (!res.ok) {
+                throw new Error('Usuário não encontrado');
+            }
+            return res.json();
         })
+        .then(function(json) {
+
+            nameElement.innerText = json.name;
+            usernameElement.innerText = json.login;
+            avatarElement.src = json.avatar_url;
+            followersElement.innerText = json.followers;
+            followingElement.innerText = json.following;
+            reposElement.innerText = json.public_repos;
+
+        })
+        .catch(function(erro) {
+            alert('Ocorreu um erro, tente novamente');
+            console.error(erro);
+        });
 
 })
